482cb1a9ad6001a7a85fad654b83d1adb17e310e
Shicheng Chu <cheng61746093@gmail.com>
2019-10-29 20:40:00 -0700

    Added Timer UI and Time Manager script
    Added Timer UI and Time Manager script

---
+++ Assets/UTech/MG-Karting/BasicAssets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    private float secondsCount;
+    private int minuteCount;
+    private int hourCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        secondsCount = 0f;
+        minuteCount = 0;
+        hourCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //set timer UI
+        secondsCount += Time.deltaTime;
+        timerText.text = hourCount + "h:" + minuteCount.ToString("00") + "m:" + ((int)secondsCount).ToString("00") + "s";
+        if (secondsCount >= 60)
+        {
+            minuteCount++;
+            secondsCount = 0;
+        }
+        else if (minuteCount >= 60)
+        {
+            hourCount++;
+            minuteCount = 0;
+        }
+    }
+}
