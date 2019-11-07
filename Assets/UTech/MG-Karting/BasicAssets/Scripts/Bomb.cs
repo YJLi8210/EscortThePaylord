f1adc2ff0498468d44a808b8ada6dbbe6301e9c3
Shicheng Chu <cheng61746093@gmail.com>
2019-11-06 16:59:41 -0800

    Added and implemented BombBox and Bomb
    Added and implemented BombBox and Bomb

---
+++ Assets/UTech/MG-Karting/BasicAssets/Scripts/Bomb.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CapsuleCollider))]
+public class Bomb : MonoBehaviour
+{
+    private HashSet<GameObject> enemies;
+    CapsuleCollider explosionRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemies = new HashSet<GameObject>();
+        explosionRange = GetComponent<CapsuleCollider>();
+        explosionRange.isTrigger = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Boom()
+    {
+        foreach(GameObject e in enemies)
+        {
+            Destroy(e);
+        }
+        enemies.Clear();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Equals("PoliceCar(Clone)") && !enemies.Contains(other.gameObject))
+        {
+            enemies.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name.Equals("PoliceCar(Clone)") && enemies.Contains(other.gameObject))
+        {
+            enemies.Remove(other.gameObject);
+        }
+    }
+}
--- Assets/UTech/MG-Karting/BasicAssets/Scripts/BombEdibleItem.cs
+++ Assets/UTech/MG-Karting/BasicAssets/Scripts/BombEdibleItem.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class BombEdibleItem : MonoBehaviour
 {
+
+    private Vector3 startPosition;
+    float timeCounter = 0f;
+    GameObject bomb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovementControl();
+
     }
+
+    private void MovementControl()
+    {
+        // rotate around local y axis
+        transform.RotateAround(transform.position, transform.up, Time.deltaTime * 90f);
+        // move upward and downward
+        timeCounter += Time.deltaTime;
+        Vector3 updatedPosition = startPosition + new Vector3(0f, .6f * Mathf.Sin(timeCounter), 0f);
+        transform.position = updatedPosition;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.tag);
+
+        if (other.gameObject.tag.Equals("Player") || other.gameObject.name.Equals("Kart"))
+        {
+            bomb = GameObject.Find("Bomb");
+            if(bomb != null)
+            {
+                bomb.GetComponent<Bomb>().Boom();
+            }
+            this.gameObject.SetActive(false);
+            Destroy(gameObject);
+
+        }
+    }
+
 }
