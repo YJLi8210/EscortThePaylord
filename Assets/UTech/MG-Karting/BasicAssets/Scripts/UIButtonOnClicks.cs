e1682068dca39de30ab2e0c07f4664390d61151c
AnkaPhantom <ankaphantom0223@gmail.com>
2019-11-15 18:19:16 +0800

    Fixed bug of reloading system

--- Assets/UTech/MG-Karting/BasicAssets/Scripts/UIButtonOnClicks.cs
+++ Assets/UTech/MG-Karting/BasicAssets/Scripts/UIButtonOnClicks.cs
@@ -5,29 +5,59 @@ using UnityEngine;
 public class UIButtonOnClicks : MonoBehaviour
 {
     public GameObject myBulletHolder;
+
+    public int MaxAmmo = 10;
+    public int CurrentAmmo;
+    public float ReloadTime = 5.0f;
+    private bool isReloading = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        CurrentAmmo = MaxAmmo;
+    }
+
+    void OnEnable()
+    {
+        isReloading = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void FireOnClick()
     {
-        if(myBulletHolder == null)
+        if (isReloading)
+            return;
+
+        if (CurrentAmmo <= 0)
         {
+            StartCoroutine(Reload());
             return;
         }
+
+        CurrentAmmo--;
+
+        if (myBulletHolder == null)
+            return;
+
         Shooting shootScript = myBulletHolder.GetComponent<Shooting>();
+
         if(shootScript == null)
-        {
             return;
-        }
+
         shootScript.Fire();
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        yield return new WaitForSeconds(ReloadTime);
+
+        CurrentAmmo = MaxAmmo;
+        isReloading = false;
     }
 }
