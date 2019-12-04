0c7f400f2f88d9e5e0832aa9c25972c137a791e6
AnkaPhantom <ankaphantom0223@gmail.com>
2019-12-04 10:38:51 +0800

    Item modification

---
+++ Assets/UTech/MG-Karting/BasicAssets/Scripts/SpawnItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnItem : MonoBehaviour
+{
+    public Transform[] SpawnPoints;
+    public GameObject Items;
+    public float spawntime = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawning", spawntime, spawntime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Spawning()
+    {
+        int spawnindex = Random.Range(0, SpawnPoints.Length);
+        Instantiate(Items, SpawnPoints[spawnindex].position, SpawnPoints[spawnindex].rotation);
+    }
+}
